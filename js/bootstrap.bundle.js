4490fa3ab7a9ec1ed876fed6280f34b523fdb597
Kasai Kirishima <o2053017@gmail.com>
2026-04-21 17:49:20 +0300

    1111
    111

---
+++ js/bootstrap.bundle.js
@@ -0,0 +1,8 @@
+document.getElementById('menuBtn').onclick=function(){
+let m=document.getElementById('menu');
+m.style.display=m.style.display=='block'?'none':'block';
+}
+document.getElementById('dropBtn').onclick=function(){
+let d=document.getElementById('dropdown');
+d.style.display=d.style.display=='block'?'none':'block';
+}
